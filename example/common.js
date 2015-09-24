8f9e5147baf1447cd946585dbf380530f4d912ae
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2015-09-23 19:18:06 -0700

    updated example

--- example/common.js
+++ example/common.js
@@ -1,4 +1,14 @@
 var AutoFormatter = require('../src/auto-formatter');
 
-var autoFormatter = new AutoFormatter(document.querySelector('.input-field'));
-autoFormatter.enableFormatting();
+var phoneNumberFormatter;
+var creditCardFormatter;
+var birthdayFormatter;
+
+phoneNumberFormatter = new AutoFormatter(document.querySelector('#phone-number'), true);
+phoneNumberFormatter.enableFormatting();
+
+creditCardFormatter = new AutoFormatter(document.querySelector('#credit-card'), true);
+creditCardFormatter.enableFormatting();
+
+birthdayFormatter = new AutoFormatter(document.querySelector('#birthday'), true);
+birthdayFormatter.enableFormatting();
