25fdee34bb0eb5fe026e1f18a944950e7c36f729
Sarbbottam Bandyopadhyay <sarbbottam@gmail.com>
2015-10-05 13:19:51 +0530

    💄  updated example

--- example/common.js
+++ example/common.js
@@ -4,11 +4,22 @@ var phoneNumberFormatter;
 var creditCardFormatter;
 var birthdayFormatter;
 
-phoneNumberFormatter = new AutoFormatter(document.querySelector('#phone-number'), true);
+var countryCodeNode = document.getElementById('country-code');
+
+phoneNumberFormatter = new AutoFormatter(document.getElementById('phone-number'), true);
 phoneNumberFormatter.enableFormatting();
 
-creditCardFormatter = new AutoFormatter(document.querySelector('#credit-card'), true);
+countryCodeNode.addEventListener('change', function() {
+  var correspondingNode = document.getElementById(this.getAttribute('data-corresponding-node-id'));
+  var format = this.options[this.selectedIndex].getAttribute('data-format');
+  correspondingNode.setAttribute('data-format', format ? format : '');
+  correspondingNode.setAttribute('placeholder', format ? format : 'Mobile number');
+  phoneNumberFormatter.enableFormatting();
+});
+
+
+creditCardFormatter = new AutoFormatter(document.getElementById('credit-card'), true);
 creditCardFormatter.enableFormatting();
 
-birthdayFormatter = new AutoFormatter(document.querySelector('#birthday'), true);
+birthdayFormatter = new AutoFormatter(document.getElementById('birthday'), true);
 birthdayFormatter.enableFormatting();
